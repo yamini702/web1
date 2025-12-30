f6271fc2d35c9ed6d52709bdb9bff18fe12120ee
yamini <yaminijanapala33@gmail.com>
2025-12-30 12:29:29 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return(
+    Scaffold(
+      appBar: AppBar(
+        title: Text("YAMINI"),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurpleAccent,
+        actions: [
+          IconButton(onPressed: (){
+            print("Notfication pressed");
+          }, icon: const Icon(Icons.notifications_active,
+          color: Colors.red,))
+        ],
+      ),
+      body:Column()
+    )
+    );
+  }
+
+}
